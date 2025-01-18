2de8c739d41fc9ed27487003d699560cd5b283af
Prasad Wable <prasadwable31@gmail.com>
2025-01-18 17:07:17 +0530

    Add files via upload

---
+++ Constant1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	const int no1 = 11;		// Allowled in c & c++
+	const int no2;   // in C++ we should always initilised by value    // Allowled in C not allowled ic c++
+	
+	return 0;
+}
